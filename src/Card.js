a3d4f2fa135537f90752f52adb716bea8732f455
beastRR <89104224+beastRR@users.noreply.github.com>
2022-10-02 21:13:07 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -15,11 +15,11 @@ const Card =({
                     <img src={image} alt="" width="250" height="280"/>
                     </div>
 
-                    <div className="col-5">
+                    <div className="col-5 mt-3">
                         <h3>{movieName}</h3>
                         <h4 className="mx-1">{director}</h4>
                         <br/>
-                        <p style={{width:120}} className="imdb"><b className=""><span className="text-">IMDb </span> {imdbRating}</b></p>
+                        <p style={{width:135}} className="imdb"><b className=""><span className="text-">IMDb </span> {imdbRating}</b></p>
                     </div>
                 </div>
             </div>
